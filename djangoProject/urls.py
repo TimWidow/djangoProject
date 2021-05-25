54f58de642d98d093074333a0dba4511e36214a4
TimWidow <Pahjkm11>
2021-05-25 22:01:48 +0300

    product_alpha_v1

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -18,8 +18,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from .views import index
-
+from main.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,5 +26,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-               urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
